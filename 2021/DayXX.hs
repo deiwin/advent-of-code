b1e980bdf2b39b9f1aac7043778894f7ee836397
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-11-30 22:38:23 +0200

    Write a script for preparing a particular problem

---
+++ 2021/DayXX.hs
@@ -0,0 +1,81 @@
+module DayXX (main) where
+
+import           Prelude                 hiding ( readFile )
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
+import           Data.Text.IO                   ( readFile )
+import           Test.HUnit.Text                ( runTestTT )
+import           Test.HUnit.Base                ( Test(TestCase)
+                                                , (@?=)
+                                                )
+import           Criterion.Main                 ( defaultMain
+                                                , bench
+                                                , whnf
+                                                )
+import           Debug.Trace                    ( traceShow
+                                                , traceShowId
+                                                )
+import qualified Text.Megaparsec               as P
+import qualified Text.Megaparsec.Char          as P
+import qualified Text.Megaparsec.Char.Lexer    as PL
+import           Data.Sequence                  ( Seq(..)
+                                                , (|>)
+                                                , (<|)
+                                                )
+import qualified Data.Sequence                 as Seq
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as S
+import           Data.IntSet                    ( IntSet )
+import qualified Data.IntSet                   as IS
+import           Data.Map.Strict                ( Map )
+import qualified Data.Map.Strict               as M
+import           Data.IntMap                    ( IntMap )
+import qualified Data.IntMap                   as IM
+import           Data.Array.IArray              ( Array )
+import qualified Data.Array.IArray             as A
+import           Data.Ix                        ( range
+                                                , inRange
+                                                )
+import           Linear.V2                      ( V2(..) )
+import           Linear.V3                      ( V3(..) )
+import           Linear.V4                      ( V4(..) )
+import           Control.Applicative            ( empty )
+import           Data.Void                      ( Void )
+import           Data.List                      ( foldl1'
+                                                , foldl'
+                                                , isPrefixOf
+                                                , iterate
+                                                )
+import           Data.Ord                       ( comparing )
+import           Data.Function                  ( (&) )
+import           Control.Arrow                  ( (>>>), second )
+import           Data.Maybe                     ( isJust
+                                                , catMaybes
+                                                )
+import           Control.Monad                  ( guard )
+
+type Parser = P.Parsec Void Text
+
+parse :: Text -> _
+parse input = case P.parse parser "" input of
+    Left  bundle -> error (P.errorBundlePretty (bundle :: P.ParseErrorBundle Text Void))
+    Right result -> result
+  where
+    parser = do
+        foo <- P.some P.letterChar
+        return foo
+    spaceConsumer :: Parser ()
+    spaceConsumer = PL.space (P.skipSome (P.char ' ')) empty empty
+    lexeme        = PL.lexeme spaceConsumer
+    symbol        = PL.symbol spaceConsumer
+    number        = lexeme PL.decimal
+    singleEol :: Parser (P.Tokens Text)
+    singleEol = P.eol <* P.notFollowedBy P.eol
+
+main = do
+    input <- readFile "inputs/DayXX.txt"
+    -- exampleInput <- readFile "inputs/DayXX_example.txt"
+    -- print $ parse input
+    runTestTT $ TestCase $ do
+        1 @?= 2
+        1 @?= 1
---
+++ 2021/prep.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [ $# -eq 0 ]; then
+  day="$(date '+%d' | xargs printf '%02d')"
+
+  echo "Creating code file ..."
+  cp DayXX.hs "Day${day}.hs"
+  sed -i '' -e "s/XX/${day}/" "Day${day}.hs"
+
+  echo "Waiting for input .."
+  mkdir -p inputs
+  aocdl -output "inputs/Day${day}.txt" -wait
+
+  echo "Watching the main function .."
+  ghcid --test='main' -W -c "stack ghci --ghc-options='-w' Day${day}.hs"
+elif [ $# -eq 2 ]; then
+  year="$1"
+  day="$(printf '%02d' $2)"
+  name="Y${year}D${day}"
+
+  echo "Creating code file ..."
+  mkdir -p "Practice"
+  cp DayXX.hs "Practice/${name}.hs"
+  sed -i '' -e "s/module DayXX/module Practice.${name}/" "Practice/${name}.hs"
+  sed -i '' -e "s|inputs/DayXX|Practice/inputs/${name}|" "Practice/${name}.hs"
+
+  echo "Waiting for input .."
+  mkdir -p "Practice/inputs"
+  aocdl -output "Practice/inputs/${name}.txt" -day "$day" -year "$year"
+
+  echo "Watching the main function .."
+  ghcid --test='main' -W -c "stack ghci --ghc-options='-w' Practice/${name}.hs"
+else
+  echo "I don't understand" >&2
+  exit 1
+fi
