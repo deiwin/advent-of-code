311c923e3c40bc48749189eb465dd6cca0d98a13
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-01 07:13:05 +0200

    Solve part 2 of day 1 and clean up

--- 2021/Day01.hs
+++ 2021/Day01.hs
@@ -1,85 +1,34 @@
 module Day01 (main) where
 
-import Control.Applicative (empty)
-import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as A
 import Data.Function ((&))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
-import qualified Data.List as L
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as S
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.IO (readFile)
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
-import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
-import Test.HUnit.Base
-  ( Test (TestCase),
-    (@?=),
-  )
+import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
-import Text.Megaparsec ((<|>))
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PL
 import Prelude hiding (readFile)
-
-type Parser = P.Parsec Void Text
 
 parse :: Text -> [Int]
 parse input = read . T.unpack <$> T.lines input
 
-solve :: _
-solve input = input
-              & zipWith (-) (tail input)
-              & filter (> 0)
-              & length
+solve :: [Int] -> Int
+solve = countIncreases
+
+countIncreases :: [Int] -> Int
+countIncreases xs =
+  xs
+    & zipWith (-) (tail xs)
+    & filter (> 0)
+    & length
+
+solve2 :: [Int] -> Int
+solve2 input = countIncreases sum
+  where
+    sum = zipWith3 (\a b c -> a + b + c) input (tail input) (tail (tail input))
 
 main = do
   input <- readFile "inputs/Day01.txt"
-  -- exampleInput <- readFile "inputs/Day01_example.txt"
-  print $ solve $ parse input
-  print $ solve $ [199, 200, 208]
   runTestTT $
     TestCase $ do
-      1 @?= 2
-      1 @?= 1
+      solve (parse input) @?= 1527
+      solve2 (parse input) @?= 1575
