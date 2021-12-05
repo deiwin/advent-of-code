1e48eb116a325c2a9f3606b29e6bc79a1fa9b28e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-05 07:55:49 +0200

    Clean up day 5 of 2021

--- 2021/Day05.hs
+++ 2021/Day05.hs
@@ -1,66 +1,19 @@
 module Day05 (main) where
 
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as A
 import qualified Data.Char as C
 import Data.Function ((&))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-    rangeSize,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
+import Data.Ix (inRange, range)
 import qualified Data.List as L
-import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
+import Data.Maybe (fromJust)
 import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import qualified Text.ParserCombinators.ReadP as P
-import Control.Monad (ap)
-import Data.Tuple (swap)
 
-parse :: String -> _
+type Line = (V2 Int, V2 Int)
+
+parse :: String -> [Line]
 parse input = run $ do
   line `P.endBy1` eol <* P.eof
   where
@@ -73,18 +26,14 @@ parse input = run $ do
       y <- number
       return (V2 x y)
     -- Standard parsers
-    letter = P.satisfy C.isLetter
     number :: P.ReadP Int
     number = read <$> P.munch1 C.isDigit
-    spaces = P.many1 (P.char ' ')
     eol = P.char '\n'
     run p = fullMatch $ P.readP_to_S p input
-    longestMatch :: [(a, [b])] -> (a, [b])
-    longestMatch = L.minimumBy (comparing (length . snd))
     fullMatch :: [(a, [b])] -> a
     fullMatch = fst . fromJust . L.find (L.null . snd)
 
-solve1 :: _
+solve1 :: [Line] -> Int
 solve1 input =
   input
     & filter isHOrV
@@ -94,15 +43,8 @@ solve1 input =
     & M.fromListWith (+)
     & M.filter (> 1)
     & M.size
-  where
-    sort (from, to)
-      | from < to = (from, to)
-      | otherwise = (to, from)
-    isHOrV x = isHorizontal x || isVertical x
-    isHorizontal (V2 x1 _, V2 x2 _) = x1 == x2
-    isVertical (V2 _ y1, V2 _ y2) = y1 == y2
 
-solve2 :: [(V2 Int, V2 Int)] -> _
+solve2 :: [Line] -> Int
 solve2 input =
   input
     & fmap sort
@@ -111,11 +53,14 @@ solve2 input =
     & M.fromListWith (+)
     & M.filter (> 1)
     & M.size
-  where
+
+specialRange :: Line -> [V2 Int]
 specialRange line
   | isHOrV line = range line
   | otherwise = diagonalRange line
-diagonalRange line@(from@(V2 x1 y1), to@(V2 x2 y2)) =
+
+diagonalRange :: Line -> [V2 Int]
+diagonalRange (from@(V2 x1 y1), to@(V2 x2 y2)) =
   from
     & iterate (+ step)
     & takeWhile (inRange bounds)
@@ -124,23 +69,28 @@ diagonalRange line@(from@(V2 x1 y1), to@(V2 x2 y2)) =
     step = V2 (deltaX `div` deltaGcd) (deltaY `div` deltaGcd)
     deltaGcd = gcd (abs deltaX) (abs deltaY)
     (V2 deltaX deltaY) = to - from
+
+sort :: Line -> Line
 sort (from, to)
   | from < to = (from, to)
   | otherwise = (to, from)
+
+isHOrV :: Line -> Bool
 isHOrV x = isHorizontal x || isVertical x
-isHorizontal (V2 x1 _, V2 x2 _) = x1 == x2
-isVertical (V2 _ y1, V2 _ y2) = y1 == y2
+  where
+    isHorizontal (V2 x1 _, V2 x2 _) = x1 == x2
+    isVertical (V2 _ y1, V2 _ y2) = y1 == y2
 
 main = do
   input <- readFile "inputs/Day05.txt"
   exampleInput <- readFile "inputs/Day05_example.txt"
-  print $ solve2 $ parse input
   runTestTT $
     TestCase $ do
-      range (V2 950 698,V2 597 698) @?= []
-      (range (V2 597 698,V2 950 698) /= [])  @?= True
-      (V2 597 698 < V2 950 698)  @?= True
+      range (V2 950 698, V2 597 698) @?= []
+      (range (V2 597 698, V2 950 698) /= []) @?= True
+      (V2 597 698 < V2 950 698) @?= True
       specialRange (V2 1 1, V2 3 3) @?= [V2 1 1, V2 2 2, V2 3 3]
       specialRange (sort (V2 9 7, V2 7 9)) @?= [V2 7 9, V2 8 8, V2 9 7]
-
       solve1 (parse input) @?= 7269
+      solve2 (parse exampleInput) @?= 12
+      solve2 (parse input) @?= 21140
