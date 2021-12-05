8c4b6db69e2531af3e8aa3c94449f038c45d8ca4
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-05 07:45:43 +0200

    Solve part 2

--- 2021/Day05.hs
+++ 2021/Day05.hs
@@ -102,14 +102,45 @@ solve1 input =
     isHorizontal (V2 x1 _, V2 x2 _) = x1 == x2
     isVertical (V2 _ y1, V2 _ y2) = y1 == y2
 
+solve2 :: [(V2 Int, V2 Int)] -> _
+solve2 input =
+  input
+    & fmap sort
+    & concatMap specialRange
+    & flip zip (repeat 1)
+    & M.fromListWith (+)
+    & M.filter (> 1)
+    & M.size
+  where
+specialRange line
+  | isHOrV line = range line
+  | otherwise = diagonalRange line
+diagonalRange line@(from@(V2 x1 y1), to@(V2 x2 y2)) =
+  from
+    & iterate (+ step)
+    & takeWhile (inRange bounds)
+  where
+    bounds = (V2 (min x1 x2) (min y1 y2), V2 (max x1 x2) (max y1 y2))
+    step = V2 (deltaX `div` deltaGcd) (deltaY `div` deltaGcd)
+    deltaGcd = gcd (abs deltaX) (abs deltaY)
+    (V2 deltaX deltaY) = to - from
+sort (from, to)
+  | from < to = (from, to)
+  | otherwise = (to, from)
+isHOrV x = isHorizontal x || isVertical x
+isHorizontal (V2 x1 _, V2 x2 _) = x1 == x2
+isVertical (V2 _ y1, V2 _ y2) = y1 == y2
+
 main = do
   input <- readFile "inputs/Day05.txt"
-  -- exampleInput <- readFile "inputs/Day05_example.txt"
-  print $ solve1 $ parse input
+  exampleInput <- readFile "inputs/Day05_example.txt"
+  print $ solve2 $ parse input
   runTestTT $
     TestCase $ do
       range (V2 950 698,V2 597 698) @?= []
       (range (V2 597 698,V2 950 698) /= [])  @?= True
-      (V2 597 698 > V2 950 698)  @?= True
+      (V2 597 698 < V2 950 698)  @?= True
+      specialRange (V2 1 1, V2 3 3) @?= [V2 1 1, V2 2 2, V2 3 3]
+      specialRange (sort (V2 9 7, V2 7 9)) @?= [V2 7 9, V2 8 8, V2 9 7]
 
-      1 @?= 1
+      solve1 (parse input) @?= 7269
