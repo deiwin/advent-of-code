5e1d3d77eefa1d72d8bd3c87cb6b6983f097b2bc
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-07 07:32:56 +0200

    More cleanup

--- 2021/Day07.hs
+++ 2021/Day07.hs
@@ -1,85 +1,26 @@
 module Day07 (main) where
 
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as A
-import qualified Data.Char as C
-import Data.Function ((&))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
-import qualified Data.List as L
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
-import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
+import Data.Text (Text, splitOn, unpack)
+import Data.Text.IO (readFile)
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
-import qualified Text.ParserCombinators.ReadP as P
+import Prelude hiding (readFile)
 
-parse :: String -> [Int]
-parse input = run $ number `P.sepBy1` P.char ',' <* eol <* P.eof
-  where
-    -- Standard parsers
-    number :: P.ReadP Int
-    number = read <$> P.munch1 C.isDigit
-    eol = P.char '\n'
-    run p = fullMatch $ P.readP_to_S p input
-    fullMatch :: [(a, [b])] -> a
-    fullMatch = fst . fromJust . L.find (L.null . snd)
+parse :: Text -> [Int]
+parse input = read . unpack <$> splitOn "," input
 
 solve1 :: [Int] -> Int
 solve1 input = minimum (flip cost input <$> range)
   where
     cost midPoint = sum . fmap (abs . (midPoint -))
-    range = [(minimum input)..(maximum input)]
+    range = [(minimum input) .. (maximum input)]
 
 solve2 :: [Int] -> Int
 solve2 input = minimum (flip cost input <$> range)
   where
     cost midPoint = sum . fmap (newCost . abs . (midPoint -))
     newCost x = ((1 + x) * x) `div` 2
-    range = [(minimum input)..(maximum input)]
+    range = [(minimum input) .. (maximum input)]
 
 main = do
   input <- readFile "inputs/Day07.txt"
