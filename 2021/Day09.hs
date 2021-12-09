b7e93ee44ee1db96409cf8dc959a25a897ad1d8c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-09 08:00:22 +0200

    Clean up day 9

--- 2021/Day09.hs
+++ 2021/Day09.hs
@@ -1,63 +1,20 @@
 module Day09 (main) where
 
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
 import Data.Array.IArray (Array)
 import qualified Data.Array.IArray as A
 import qualified Data.Char as C
 import Data.Function ((&))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
+import Data.Ix (inRange, range)
 import qualified Data.List as L
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
+import Data.Maybe (fromJust)
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
 import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import qualified Text.ParserCombinators.ReadP as P
 
-parse :: String -> _
+parse :: String -> [[Int]]
 parse input = run $ P.many1 digit `P.endBy1` eol <* P.eof
   where
     -- Standard parsers
@@ -68,7 +25,7 @@ parse input = run $ P.many1 digit `P.endBy1` eol <* P.eof
     fullMatch :: [(a, [b])] -> a
     fullMatch = fst . fromJust . L.find (L.null . snd)
 
-solve1 :: _
+solve1 :: [[Int]] -> Int
 solve1 input =
   range bounds
     & filter isLowPoint
@@ -84,7 +41,7 @@ solve1 input =
     array = A.listArray bounds (concat input)
     bounds = (V2 0 0, V2 (length input - 1) (length (head input) - 1))
 
-solve2 :: _
+solve2 :: [[Int]] -> Int
 solve2 input =
   range bounds
     & filter isLowPoint
@@ -126,19 +83,9 @@ solve2 input =
     array = A.listArray bounds (concat input)
     bounds = (V2 0 0, V2 (length input - 1) (length (head input) - 1))
 
-showGrid :: Array (V2 Int) Int -> String
-showGrid grid = unlines rows
-  where
-    bounds = A.bounds grid
-    (_, V2 maxY maxX) = bounds
-    rows = showRow <$> [0 .. maxY]
-    showRow y = concatMap (showCell y) [0 .. maxX]
-    showCell y x = show $ grid A.! V2 y x
-
 main = do
   input <- readFile "inputs/Day09.txt"
   exampleInput <- readFile "inputs/Day09_example.txt"
-  print $ solve2 $ parse input
   runTestTT $
     TestCase $ do
       solve1 (parse exampleInput) @?= 15
