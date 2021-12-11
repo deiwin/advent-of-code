57674c08691d704e54f43c79840febb2694debac
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-11 08:23:34 +0200

    Clean up day 11

--- 2021/Day11.hs
+++ 2021/Day11.hs
@@ -1,89 +1,37 @@
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE FlexibleContexts #-}
-
 module Day11 (main) where
 
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as A
-import qualified Data.Char as C
-import Data.Function ((&))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-    rangeSize
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
-import qualified Data.List as L
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    isJust,
-    maybe,
-    fromMaybe,
-    isNothing
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
-import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
-import Test.HUnit.Base (Test (TestCase), (@?=))
-import Test.HUnit.Text (runTestTT)
-import qualified Text.ParserCombinators.ReadP as P
 import Control.Arrow (second)
 import Control.Monad (liftM2)
+import Data.Array.IArray (Array)
+import qualified Data.Array.IArray as A
+import Data.Function ((&))
+import Data.Ix (inRange, rangeSize)
+import Data.List (foldl')
+import qualified Data.List as L
+import Data.Maybe (fromMaybe, isNothing)
+import Linear.V2 (V2 (..))
+import Test.HUnit.Base (Test (TestCase), (@?=))
+import Test.HUnit.Text (runTestTT)
 
 type Octopus = Maybe Int
+
 type Grid = Array (V2 Int) Octopus
 
 parse :: String -> [[Int]]
-parse = lines >>> fmap (fmap (read . (: [])))
+parse = fmap (fmap (read . (: []))) . lines
 
-solve1 :: _
+solve1 :: [[Int]] -> Int
 solve1 input = fst $ playNRounds 100 array
   where
-    playNRounds n arr = foldl' go (0, arr) [1..n]
+    playNRounds n arr = foldl' go (0, arr) [1 .. n]
       where
         go (x, arr) _ = case playRound arr of
-                          (y, newArr) -> (x + y, newArr)
+          (y, newArr) -> (x + y, newArr)
     array :: Array (V2 Int) Octopus
     array = A.listArray bounds (Just <$> concat input)
     bounds = (V2 0 0, V2 (length input - 1) (length (head input) - 1))
 
-solve2 :: _
+solve2 :: [[Int]] -> Maybe Int
 solve2 input =
   (0, array)
     & iterate (playRound . snd)
@@ -121,10 +69,10 @@ playRound arr = (resetCount, resetArr)
           toFlash
             & concatMap (surrounding arr . fst)
             & flip zip (repeat (Just 1))
-    increasedArr =
-      range (A.bounds arr)
-        & flip zip (repeat (Just 1))
-        & A.accum (liftM2 (+)) arr
+    increasedArr :: Grid
+    increasedArr = A.amap (fmap (+ 1)) arr
+
+surrounding :: Grid -> V2 Int -> [V2 Int]
 surrounding arr coord =
   [ V2 (-1) 0,
     V2 1 0,
