7cb5745445e2fd074930ee0fd971cf260cfa5ffd
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-12 08:15:04 +0200

    Solve part 2

--- 2021/Day12.hs
+++ 2021/Day12.hs
@@ -12,6 +12,7 @@ import Data.Array.IArray (Array)
 import qualified Data.Array.IArray as A
 import qualified Data.Char as C
 import Data.Function ((&))
+import qualified Data.Graph as G
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import Data.IntSet (IntSet)
@@ -32,6 +33,7 @@ import qualified Data.Map.Strict as M
 import Data.Maybe
   ( catMaybes,
     fromJust,
+    fromMaybe,
     isJust,
   )
 import Data.Ord (comparing)
@@ -43,6 +45,7 @@ import Data.Sequence
 import qualified Data.Sequence as Seq
 import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Tuple (swap)
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as VU
 import Data.Void (Void)
@@ -56,14 +59,11 @@ import Linear.V4 (V4 (..))
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import qualified Text.ParserCombinators.ReadP as P
-import Data.Tuple (swap)
-import Control.Arrow (second)
-
-import qualified Data.Graph as G
-
 
 type Edge = (String, String)
+
 type Graph = Map String (Set String)
+
 type Path = [String]
 
 parse :: String -> [Edge]
@@ -86,10 +86,10 @@ solve1 input = length $ dff graph
     dff :: Graph -> [Path]
     dff = go "start" []
       where
-        go from@"end" visited _ = [from:visited]
+        go from@"end" visited _ = [from : visited]
         go from visited g = concatMap (\to -> go to newVisited g) toVisit
           where
-            newVisited = from:visited
+            newVisited = from : visited
             toVisit =
               from
                 & (g M.!)
@@ -107,11 +107,58 @@ solve1 input = length $ dff graph
         & fmap (second S.singleton)
         & M.fromListWith S.union
 
+solve2 :: _
+solve2 input = length $ dff graph
+  where
+    dff :: Graph -> [Path]
+    dff = go "start" []
+      where
+        go from@"end" visited _ = [from : visited]
+        go from visited g = concatMap (\to -> go to newVisited g) toVisit
+          where
+            newVisited = from : visited
+            toVisit =
+              from
+                & (g M.!)
+                & S.toList
+                & filter visitable
+            visitable to =
+              all C.isUpper to
+                || canVisitSmallCave
+              where
+                canVisitSmallCave = notVisitedYet || canVisitTwice
+                notVisitedYet = fromMaybe 0 (M.lookup to visitedCountMap) < 1
+                canVisitTwice =
+                  to `notElem` ["start", "end"]
+                    && noSmallCaveHasBeenVisitedTwiceYet
+                noSmallCaveHasBeenVisitedTwiceYet =
+                  visitedCountMap
+                    & M.filterWithKey (\k _ -> all C.isLower k)
+                    & M.filter (> 1)
+                    & M.null
+                visitedCountMap =
+                  newVisited
+                    & flip zip (repeat 1)
+                    & M.fromListWith (+)
+    graph :: Graph
+    graph =
+      input
+        & (++ (swap <$> input))
+        & fmap (second S.singleton)
+        & M.fromListWith S.union
+
 main = do
   input <- readFile "inputs/Day12.txt"
-  exampleInput <- readFile "inputs/Day12_example.txt"
-  print $ solve1 $ parse input
+  exampleInput1 <- readFile "inputs/Day12_example1.txt"
+  exampleInput2 <- readFile "inputs/Day12_example2.txt"
+  exampleInput3 <- readFile "inputs/Day12_example3.txt"
   runTestTT $
     TestCase $ do
-      solve1 (parse exampleInput) @?= 226
+      solve1 (parse exampleInput1) @?= 10
+      solve1 (parse exampleInput2) @?= 19
+      solve1 (parse exampleInput3) @?= 226
       solve1 (parse input) @?= 4885
+      solve2 (parse exampleInput1) @?= 36
+      solve2 (parse exampleInput2) @?= 103
+      solve2 (parse exampleInput3) @?= 3509
+      solve2 (parse input) @?= 4885
