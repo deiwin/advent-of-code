9bc67b00ea1a59bf034d11a5a7f630d4e79340c9
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-12 08:27:26 +0200

    Clean up day 12

--- 2021/Day12.hs
+++ 2021/Day12.hs
@@ -1,61 +1,15 @@
 module Day12 (main) where
 
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as A
+import Control.Arrow (second)
 import qualified Data.Char as C
 import Data.Function ((&))
-import qualified Data.Graph as G
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
 import qualified Data.List as L
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    fromMaybe,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Tuple (swap)
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
-import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import qualified Text.ParserCombinators.ReadP as P
@@ -64,7 +18,7 @@ type Edge = (String, String)
 
 type Graph = Map String (Set String)
 
-type Path = [String]
+type Visited = Map String Int
 
 parse :: String -> [Edge]
 parse input = run (edge `P.endBy1` eol <* P.eof)
@@ -80,72 +34,52 @@ parse input = run (edge `P.endBy1` eol <* P.eof)
     fullMatch :: [(a, [b])] -> a
     fullMatch = fst . fromJust . L.find (L.null . snd)
 
-solve1 :: _
-solve1 input = length $ dff graph
+solve1 :: [Edge] -> Int
+solve1 input = length $ dffWith visitable graph
   where
-    dff :: Graph -> [Path]
-    dff = go "start" []
-      where
-        go from@"end" visited _ = [from : visited]
-        go from visited g = concatMap (\to -> go to newVisited g) toVisit
-          where
-            newVisited = from : visited
-            toVisit =
-              from
-                & (g M.!)
-                & S.toList
-                & filter visitable
-            visitable to
-              | all C.isUpper to = True
-              | to `L.elem` visited = False
-              | otherwise = True
+    visitable visited toVisit
+      | all C.isUpper toVisit = True
+      | toVisit `M.member` visited = False
+      | otherwise = True
+    graph = mkGraph input
 
-    graph :: Graph
-    graph =
-      input
-        & (++ (swap <$> input))
-        & fmap (second S.singleton)
-        & M.fromListWith S.union
-
-solve2 :: _
-solve2 input = length $ dff graph
+solve2 :: [Edge] -> Int
+solve2 input = length $ dffWith visitable graph
   where
-    dff :: Graph -> [Path]
-    dff = go "start" []
+    visitable visited toVisit = isBigCave || canVisitSmallCave
       where
-        go from@"end" visited _ = [from : visited]
-        go from visited g = concatMap (\to -> go to newVisited g) toVisit
-          where
-            newVisited = from : visited
-            toVisit =
-              from
-                & (g M.!)
-                & S.toList
-                & filter visitable
-            visitable to =
-              all C.isUpper to
-                || canVisitSmallCave
-              where
-                canVisitSmallCave = notVisitedYet || canVisitTwice
-                notVisitedYet = fromMaybe 0 (M.lookup to visitedCountMap) < 1
-                canVisitTwice =
-                  to `notElem` ["start", "end"]
-                    && noSmallCaveHasBeenVisitedTwiceYet
-                noSmallCaveHasBeenVisitedTwiceYet =
-                  visitedCountMap
-                    & M.filterWithKey (\k _ -> all C.isLower k)
-                    & M.filter (> 1)
-                    & M.null
-                visitedCountMap =
-                  newVisited
-                    & flip zip (repeat 1)
-                    & M.fromListWith (+)
-    graph :: Graph
-    graph =
-      input
-        & (++ (swap <$> input))
-        & fmap (second S.singleton)
-        & M.fromListWith S.union
+        isBigCave = all C.isUpper toVisit
+        canVisitSmallCave = notVisitedYet || canVisitTwice
+        notVisitedYet = fromMaybe 0 (M.lookup toVisit visited) < 1
+        canVisitTwice =
+          toVisit `notElem` ["start", "end"]
+            && noSmallCaveHasBeenVisitedTwiceYet
+        noSmallCaveHasBeenVisitedTwiceYet =
+          visited
+            & M.filterWithKey (\k _ -> all C.isLower k)
+            & M.filter (> 1)
+            & M.null
+    graph = mkGraph input
+
+dffWith :: (Visited -> String -> Bool) -> Graph -> [Visited]
+dffWith visitable = go "start" M.empty
+  where
+    go from@"end" visited _ = [M.insertWith (+) from 1 visited]
+    go from visited graph = concatMap (\to -> go to newVisited graph) toVisit
+      where
+        newVisited = M.insertWith (+) from 1 visited
+        toVisit =
+          from
+            & (graph M.!)
+            & S.toList
+            & filter (visitable newVisited)
+
+mkGraph :: [Edge] -> Graph
+mkGraph input =
+  input
+    & (++ (swap <$> input))
+    & fmap (second S.singleton)
+    & M.fromListWith S.union
 
 main = do
   input <- readFile "inputs/Day12.txt"
@@ -161,4 +95,4 @@ main = do
       solve2 (parse exampleInput1) @?= 36
       solve2 (parse exampleInput2) @?= 103
       solve2 (parse exampleInput3) @?= 3509
-      solve2 (parse input) @?= 4885
+      solve2 (parse input) @?= 117095
