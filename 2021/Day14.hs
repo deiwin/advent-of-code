773ab57a9b1558cf3bff2e1a500aacedf60bda02
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-14 09:53:51 +0200

    Clean up day 14

--- 2021/Day14.hs
+++ 2021/Day14.hs
@@ -2,70 +2,22 @@
 
 module Day14 (main) where
 
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (first, second, (>>>))
-import Control.Monad (guard)
+import Control.Arrow (first)
 import Control.Monad.Memo (MonadMemo, for2, memo, runMemo)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import qualified Data.Array.IArray as A
 import qualified Data.Char as C
-import Data.Function (fix, (&))
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
-  ( inRange,
-    range,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
+import Data.Function ((&))
+import Data.List (foldl')
 import qualified Data.List as L
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
-import qualified Data.Sequence as Seq
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
-import Linear.V2 (V2 (..))
-import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
+import Data.Maybe (fromJust)
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import qualified Text.ParserCombinators.ReadP as P
 
 type Rule = ((Char, Char), Char)
 
-type Rules = Map (Char, Char) Char
-
-type Cache = Map (Int, (Char, Char)) (Map Char Integer)
-
+type Cache = Map (Int, (Char, Char)) (Map Char Int)
 
 parse :: String -> (String, [Rule])
 parse input = run $ do
@@ -85,25 +37,24 @@ parse input = run $ do
     fullMatch :: [(a, [b])] -> a
     fullMatch = fst . fromJust . L.find (L.null . snd)
 
-solve1 :: (String, [Rule]) -> Integer
+solve1 :: (String, [Rule]) -> Int
 solve1 (polymer, ruleL) = score (playNRounds ruleL 10 polymer)
 
-solve2 :: _
+solve2 :: (String, [Rule]) -> Int
 solve2 (polymer, ruleL) = score (playNRounds ruleL 40 polymer)
 
-score :: Map a Integer -> Integer
+score :: Map a Int -> Int
 score polymerM = maximum polymerL - minimum polymerL
   where
     polymerL = snd <$> M.toList polymerM
 
-playNRounds :: [Rule] -> Int -> String -> Map Char Integer
+playNRounds :: [Rule] -> Int -> String -> Map Char Int
 playNRounds ruleL n polymer =
   pairs
     & foldl' go (M.empty, M.empty)
     & fst
     & M.unionWith (+) corrections
   where
-    rules = M.fromList ruleL
     corrections =
       middles
         & flip zip (repeat (-1))
@@ -116,16 +67,17 @@ playNRounds ruleL n polymer =
     go (acc, cache) p =
       playN n p cache
         & first (M.unionWith (+) acc)
-    playN :: Int -> (Char, Char) -> Cache -> (Map Char Integer, Cache)
+    ruleM = M.fromList ruleL
+    playN :: Int -> (Char, Char) -> Cache -> (Map Char Int, Cache)
     playN n p cache = runMemo (playNMemo n p) cache
     playNMemo ::
-      MonadMemo (Int, (Char, Char)) (Map Char Integer) m =>
+      MonadMemo (Int, (Char, Char)) (Map Char Int) m =>
       Int ->
       (Char, Char) ->
-      m (Map Char Integer)
+      m (Map Char Int)
     playNMemo 0 (a, b) = return (M.fromListWith (+) (zip [a, b] (repeat 1)))
     playNMemo n (a, b) =
-      case M.lookup (a, b) rules of
+      case M.lookup (a, b) ruleM of
         Nothing -> recurse 0 (a, b)
         Just c -> do
           aM <- recurse (n - 1) (a, c)
@@ -135,14 +87,6 @@ playNRounds ruleL n polymer =
             & return
       where
         recurse = for2 memo playNMemo
-
--- The head and drop 1 are necessary for avoiding duplicates. Could use a
--- special kind of concat instead but :shrug:
-playRound :: Rules -> String -> String
-playRound rules polymer = head polymer : concatMap (drop 1 . maybeInsert) pairs
-  where
-    maybeInsert p@(a, b) = maybe [a, b] (\x -> [a, x, b]) (M.lookup p rules)
-    pairs = zip polymer (drop 1 polymer)
 
 main = do
   input <- readFile "inputs/Day14.txt"
