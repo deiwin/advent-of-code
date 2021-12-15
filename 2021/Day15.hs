9cdd64bed9af07af57a2ff2f5253c63be2504d1d
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-15 08:15:25 +0200

    Solve part 2

--- 2021/Day15.hs
+++ 2021/Day15.hs
@@ -56,6 +56,8 @@ import Linear.V4 (V4 (..))
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import qualified Text.ParserCombinators.ReadP as P
+import Data.PQueue.Min (MinQueue)
+import qualified Data.PQueue.Min as MQ
 
 type Grid = Array (V2 Int) Int
 
@@ -67,28 +69,54 @@ solve1 input = aStar grid (V2 0 0) (snd (A.bounds grid))
   where
     grid = buildGrid input
 
-aStar :: Grid -> V2 Int -> V2 Int -> _
-aStar grid from to = go from 0 S.empty
+solve2 :: _
+solve2 input = aStar grid (V2 0 0) (snd (A.bounds grid))
+-- solve2 input = showGrid grid
   where
-    go :: V2 Int -> Int -> Set (Int, Int, V2 Int) -> Int
-    go c cost toVisit
+    grid = buildGrid newInput
+    newInput :: [[Int]]
+    newInput =
+      input
+        & fmap updateRow
+        & replicate 5
+        & flip zip [0..]
+        & concatMap (\(xss, d) -> fmap (addToEachWithWraparound d) xss)
+    updateRow :: [Int] -> [Int]
+    updateRow row =
+      row
+        & replicate 5
+        & zip [0..]
+        & concatMap (uncurry addToEachWithWraparound)
+    addToEachWithWraparound d xs = fmap (`add` d) xs
+    add x d
+      | x + d > 9 = x + d - 9
+      | otherwise = x + d
+
+
+aStar :: Grid -> V2 Int -> V2 Int -> _
+aStar grid from to = go from S.empty 0 MQ.empty
+  where
+    go :: V2 Int -> Set (V2 Int) -> Int -> MinQueue _ -> Int
+    go c visited cost toVisit
       | c == to = cost
       | otherwise =
-        case S.minView newToConsiderToVisit of
+        case MQ.minView newToConsiderToVisit of
           Nothing -> undefined
-          Just ((_, newCost, newC), newToVisit) -> go newC newCost newToVisit
+          Just ((_, _, newCost, newC), newToVisit) -> go newC newVisited newCost newToVisit
+          -- Just ((_, _, newCost, newC), newToVisit) ->
+          --   traceShow (newCost, newC) $ go newC newVisited newCost newToVisit
       where
         newToConsiderToVisit =
           surrounding grid c
-            -- & filter (not . (`S.member` newVisited))
+            & filter (not . (`S.member` newVisited))
             & fmap toVisitFor
-            & S.fromList
-            & S.union toVisit
-        toVisitFor c = (estCost, realCost, c)
+            & MQ.fromList
+            & MQ.union toVisit
+        toVisitFor c = (estCost, to - c, realCost, c)
           where
             realCost = cost + (grid A.! c)
             estCost = realCost + estimatedExtraCost c
-        -- newVisited = S.insert c visited
+        newVisited = S.insert c visited
     estimatedExtraCost :: V2 Int -> Int
     estimatedExtraCost c = case to - c of V2 y x -> y + x
 
@@ -103,11 +131,23 @@ buildGrid input = A.listArray bounds (concat input)
   where
     bounds = (V2 0 0, V2 (length input - 1) (length (head input) - 1))
 
+showGrid :: Grid -> String
+showGrid grid = unlines rows
+  where
+    bounds            = A.bounds grid
+    (_, V2 maxY maxX) = bounds
+    rows              = showRow <$> [0 .. maxY]
+    showRow y = concatMap (showCell y) [0 .. maxX]
+    showCell y x = show $ grid A.! V2 y x
+
 main = do
   input <- readFile "inputs/Day15.txt"
   exampleInput <- readFile "inputs/Day15_example.txt"
-  print $ solve1 $ parse exampleInput
+  print $ solve2 $ parse input
+  -- putStr $ solve2 $ parse exampleInput
   runTestTT $
     TestCase $ do
+      1 @?= 40
       solve1 (parse exampleInput) @?= 40
       solve1 (parse input) @?= 702
+      solve2 (parse input) @?= 2955
