e8c8892d0c25972dfd3245158120097bd0dec2bf
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-19 16:11:53 +0200

    Revert "Avoid checking scanner pairs more than once"
    This reverts commit a8bb83dc1dfb65da358913596049e9330ebc9e13. It's a
    decent optimization but not worth the additional complexity for the
    small improvement it provides.

--- 2021/Day19.hs
+++ 2021/Day19.hs
@@ -2,12 +2,10 @@ module Day19 (main) where
 
 import Control.Monad (guard)
 import qualified Data.Char as C
-import Data.Either (isRight, partitionEithers)
 import Data.Function ((&))
 import qualified Data.List as L
-import Data.Maybe (fromJust, listToMaybe)
+import Data.Maybe (fromJust, listToMaybe, mapMaybe)
 import qualified Data.Set as S
-import Data.Tuple (swap)
 import Debug.Trace (traceShow)
 import qualified Linear.Matrix as LM
 import Linear.V3 (V3 (..))
@@ -64,39 +62,19 @@ manhattan a b = abs x + abs y + abs z
     (V3 x y z) = a - b
 
 align :: [Report] -> [(V3 Int, Report)]
-align [] = []
-align (r : rs) = go [(V3 0 0 0, r)] S.empty rs
+align [] = undefined
+align (r : rs) = go [(V3 0 0 0, r)] rs
   where
-    go a b c | traceShow (length a, S.size b, length c) False = undefined
-    go aligned _ [] = aligned
-    go aligned knownNoMatch unaligned = go newAligned newKnownNoMatch newUnaligned
+    go a b | traceShow (length a, length b) False = undefined
+    go aligned [] = aligned
+    go aligned unaligned = go newAligned newUnaligned
       where
         newUnaligned = L.filter ((/= scannerNumber matchingReport) . scannerNumber) unaligned
         newAligned = (diff, matchingReport) : aligned
-        (noMatchPairs, (diff, matchingReport)) =
+        (diff, matchingReport) =
           cartProd (snd <$> aligned) unaligned
-            & fmap checkPair
-            & partitionUntilRight
-        checkPair (a, b)
-          | p `S.member` knownNoMatch = Left p
-          | otherwise =
-            case triangulate a b of
-              Nothing -> Left p
-              Just x -> Right x
-          where
-            p = (scannerNumber a, scannerNumber b)
-        partitionUntilRight :: [Either a b] -> ([a], b)
-        partitionUntilRight xs =
-          case L.findIndex isRight xs of
-            Nothing -> error "expecting to find at least one match"
-            Just i -> (lefts, right)
-              where
-                (lefts, [right]) = partitionEithers (take (i + 1) xs)
-        newKnownNoMatch =
-          noMatchPairs
-            & (++ (swap <$> noMatchPairs))
-            & S.fromList
-            & S.union knownNoMatch
+            & mapMaybe (uncurry triangulate)
+            & head
 
 triangulate :: Report -> Report -> Maybe (V3 Int, Report)
 triangulate base comparison = listToMaybe $ do
