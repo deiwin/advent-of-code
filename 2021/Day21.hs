b0f58d88b677d483d5cd79071ec5864aaf69dd99
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2021-12-21 13:11:34 +0200

    Solve part 2

--- 2021/Day21.hs
+++ 2021/Day21.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Day21 (main) where
 
-import Data.Either (fromLeft)
-import Control.Monad (foldM)
 import Control.Applicative (empty, (<|>))
 import Control.Arrow (second, (>>>))
-import Control.Monad (guard)
+import Control.Monad (foldM, guard)
+import Control.Monad.Memo (MonadMemo, for2, memo, runMemo, startEvalMemo)
 import Criterion.Main
   ( bench,
     defaultMain,
@@ -13,6 +14,7 @@ import Criterion.Main
 import Data.Array.IArray (Array)
 import qualified Data.Array.IArray as A
 import qualified Data.Char as C
+import Data.Either (fromLeft)
 import Data.Function ((&))
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
@@ -61,27 +63,31 @@ import qualified Text.ParserCombinators.ReadP as P
 import Text.Printf (printf)
 
 data Player = Player
-  { score :: !Int,
+  { pNumber :: !Int,
+    score :: !Int,
     pos :: !Int
   }
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
 data State = State
   { currentPlayer :: !Player,
-    nextPlayer :: !Player,
+    nextPlayer :: !Player
+  }
+  deriving (Eq, Ord, Show)
+
+data DetState = DetState
+  { state :: State,
     turnsTaken :: !Int,
     nextRolls :: [Int]
   }
-  deriving (Eq)
 
-instance Show State where
-  show state =
+instance Show DetState where
+  show x =
     printf
-      "State { currentPlayer = %s, nextPlayer = %s, turnsTaken = %d, nextRolls = %d.. }"
-      (show (currentPlayer state))
-      (show (nextPlayer state))
-      (turnsTaken state)
-      (head (nextRolls state))
+      "DetState { state = %s, turnsTaken = %d, nextRolls = %d.. }"
+      (show (state x))
+      (turnsTaken x)
+      (head (nextRolls x))
 
 parse :: String -> (Int, Int)
 parse input = run $ do
@@ -97,52 +103,77 @@ parse input = run $ do
     fullMatch :: [(a, [b])] -> a
     fullMatch = fst . fromJust . L.find (L.null . snd)
 
-solve1 :: _
-solve1 = finalScore . runEither playTurn  . initialState
+solve1 :: (Int, Int) -> Int
+solve1 = finalScore . runEither (endCondition playTurnDet) . makeState
   where
-    finalScore :: State -> Int
-    finalScore state = score (currentPlayer state) * (3 * turnsTaken state)
+    endCondition f s
+      | score (nextPlayer (state newS)) >= 1000 = Left newS
+      | otherwise = Right newS
+      where
+        newS = f s
+    finalScore detState = score (currentPlayer (state detState)) * (3 * turnsTaken detState)
+    makeState input =
+      DetState
+        { state = initialState input,
+          turnsTaken = 0,
+          nextRolls = cycle [1 .. 100]
+        }
 
 initialState :: (Int, Int) -> State
-initialState input =
-  State
-    { currentPlayer = p1,
-      nextPlayer = p2,
-      turnsTaken = 0,
-      nextRolls = cycle [1 .. 100]
-    }
+initialState input = State {currentPlayer = p1, nextPlayer = p2}
   where
-    p1 = Player {pos = p1Pos, score = 0}
-    p2 = Player {pos = p2Pos, score = 0}
+    p1 = Player {pNumber = 1, pos = p1Pos, score = 0}
+    p2 = Player {pNumber = 2, pos = p2Pos, score = 0}
     (p1Pos, p2Pos) = input
 
-runEither :: (a -> Either a a) -> a -> a
-runEither f x = fromLeft undefined (foldM (\x _ -> f x) x [0..])
+solve2 :: (Int, Int) -> _
+solve2 input = uncurry max $ countDiracWins $ initialState input
 
-playTurn :: State -> Either State State
-playTurn state
-  | score newNextPlayer >= 1000 = Left newState
-  | otherwise = Right newState
+countDiracWins :: State -> (Int, Int)
+countDiracWins = startEvalMemo . go
   where
-    newState =
-      State
-        { currentPlayer = nextPlayer state,
-          nextPlayer = newNextPlayer,
-          turnsTaken = (turnsTaken state) + 1,
-          nextRolls = newNextRolls
-        }
-    newNextPlayer = Player { score = newScore, pos = newPos }
+    go :: MonadMemo State (Int, Int) m => State -> m (Int, Int)
+    go s
+      | score (nextPlayer s) >= 21 =
+        if pNumber (nextPlayer s) == 1
+          then return (1, 0)
+          else return (0, 1)
+      | otherwise = tupleSum <$> mapM (\rs -> memo go (playTurn rs s)) rolls
+    rolls =  [[x, y, z] | x <- [1 .. 3], y <- [1 .. 3], z <- [1 .. 3]]
+    tupleSum :: [(Int, Int)] -> (Int, Int)
+    tupleSum = foldl' tupleAdd (0, 0)
+    tupleAdd (a, b) (c, d) = (a + c, b + d)
+
+runEither :: (a -> Either a a) -> a -> a
+runEither f x = fromLeft undefined (foldM (\x _ -> f x) x [0 ..])
+
+playTurnDet :: DetState -> DetState
+playTurnDet detState =
+  DetState
+    { state = playTurn rolls (state detState),
+      nextRolls = newNextRolls,
+      turnsTaken = turnsTaken detState + 1
+    }
+  where
+    (rolls, newNextRolls) = L.splitAt 3 (nextRolls detState)
+
+playTurn :: [Int] -> State -> State
+playTurn rolls state =
+  State
+    { currentPlayer = nextPlayer state,
+      nextPlayer = newNextPlayer
+    }
+  where
+    newNextPlayer = (currentPlayer state) {score = newScore, pos = newPos}
     newScore = score (currentPlayer state) + newPos
     newPos = move (pos (currentPlayer state)) (sum rolls)
     move x d = ((x + d - 1) `mod` 10) + 1
-    (rolls, newNextRolls) = L.splitAt 3 (nextRolls state)
 
 main = do
   input <- readFile "inputs/Day21.txt"
-  -- exampleInput <- readFile "inputs/Day21_example.txt"
-  -- print $ solve1 $ parse input
-  print $ solve1 (4, 8)
   runTestTT $
     TestCase $ do
       solve1 (4, 8) @?= 739785
       solve1 (parse input) @?= 512442
+      solve2 (4, 8) @?= 444356092776315
+      solve2 (parse input) @?= 346642902541848
