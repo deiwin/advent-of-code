0d192443fe6cc3ce439cc958b079706269c1c5ac
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2022-12-02 10:49:58 -0800

    Solve day 2

---
+++ 2022/Day02.hs
@@ -0,0 +1,86 @@
+module Day02 (main) where
+
+import Control.Applicative ((<|>))
+import Data.Function ((&))
+import Data.Functor (($>))
+import Data.List qualified as L
+import Data.Maybe (fromJust)
+import Test.HUnit.Base (Test (TestCase), (@?=))
+import Test.HUnit.Text (runTestTT)
+import Text.ParserCombinators.ReadP qualified as P
+
+data Hand = Rock | Paper | Scissors
+  deriving (Show, Eq, Enum, Bounded)
+
+parse :: String -> [(Hand, Hand)]
+parse input = run $ pair `P.endBy1` eol
+  where
+    pair = do
+      a <- opponent <* P.char ' '
+      b <- self
+      return (a, b)
+    -- Standard parsers
+    self =
+      P.char 'X' $> Rock
+        <|> P.char 'Y' $> Paper
+        <|> P.char 'Z' $> Scissors
+    opponent =
+      P.char 'A' $> Rock
+        <|> P.char 'B' $> Paper
+        <|> P.char 'C' $> Scissors
+    eol = P.char '\n'
+    run p = fullMatch $ P.readP_to_S p input
+    fullMatch :: [(a, [b])] -> a
+    fullMatch = fst . fromJust . L.find (L.null . snd)
+
+handScore :: Hand -> Int
+handScore = \case
+  Rock -> 1
+  Paper -> 2
+  Scissors -> 3
+
+outcomeScore :: (Hand, Hand) -> Int
+outcomeScore = \case
+  (x, y) | x == y -> tie
+  (Rock, Scissors) -> loss
+  (Scissors, Paper) -> loss
+  (Paper, Rock) -> loss
+  _ -> win
+  where
+    loss = 0
+    tie = 3
+    win = 6
+
+score :: (Hand, Hand) -> Int
+score round@(_, hand) = handScore hand + outcomeScore round
+
+solve1 :: [(Hand, Hand)] -> Int
+solve1 input =
+  input
+    & fmap score
+    & sum
+
+updateHand :: (Hand, Hand) -> (Hand, Hand)
+updateHand = \case
+  (x, Rock) -> (x, findForResult x 0)
+  (x, Paper) -> (x, x)
+  (x, Scissors) -> (x, findForResult x 6)
+  where
+    findForResult x s = fromJust $ L.find (\y -> outcomeScore (x, y) == s) allHands
+    allHands = [minBound .. maxBound]
+
+solve2 :: [(Hand, Hand)] -> Int
+solve2 input =
+  input
+    & fmap (score . updateHand)
+    & sum
+
+main = do
+  input <- readFile "inputs/Day02.txt"
+  exampleInput <- readFile "inputs/Day02_example.txt"
+  runTestTT $
+    TestCase $ do
+      solve1 (parse exampleInput) @?= 15
+      solve1 (parse input) @?= 11906
+      solve2 (parse exampleInput) @?= 12
+      solve2 (parse input) @?= 11186
