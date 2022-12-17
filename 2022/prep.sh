53f7e3a7d5b604507054274ad3d202b701274e2f
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2022-12-18 00:35:08 +0200

    Allow specifying day number

--- 2022/prep.sh
+++ 2022/prep.sh
@@ -23,6 +23,19 @@ if [ $# -eq 0 ]; then
 
   echo "Watching the main function .."
   ghcid --test='main' -W -c "stack ghci --ghc-options='-w' Day${day}.hs"
+elif [ $# -eq 1 ]; then
+  day="$(printf '%02d' "$1")"
+
+  echo "Creating code file ..."
+  cp DayXX.hs "Day${day}.hs"
+  sed -i '' -e "s/XX/${day}/" "Day${day}.hs"
+
+  echo "Waiting for input .."
+  mkdir -p inputs
+  aocdl -output "inputs/Day${day}.txt"
+
+  echo "Watching the main function .."
+  ghcid --test='main' -W -c "stack ghci --ghc-options='-w' Day${day}.hs"
 elif [ $# -eq 2 ]; then
   year="$1"
   day="$(printf '%02d' "$2")"
