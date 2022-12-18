5a15ebb74d33fb0be7320127b2e97ed169cdb020
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2022-12-18 12:29:19 +0200

    Solve day 18

--- 2022/Day18.hs
+++ 2022/Day18.hs
@@ -1,60 +1,17 @@
 module Day18 (main) where
 
-import Control.Monad.Memo (MonadMemo, memo, startEvalMemo, startRunMemo)
-import Control.Applicative (empty, (<|>))
-import Control.Arrow (first, second, (>>>))
-import Control.Monad (guard, when)
-import Criterion.Main
-  ( bench,
-    defaultMain,
-    whnf,
-  )
-import Data.Array.IArray (Array)
-import Data.Array.IArray qualified as A
 import Data.Char qualified as C
 import Data.Function ((&))
-import Data.Functor (($>), (<$), (<&>))
-import Data.IntMap (IntMap)
-import Data.IntMap qualified as IM
-import Data.IntSet (IntSet)
-import Data.IntSet qualified as IS
-import Data.Ix
-  ( inRange,
-    range,
-  )
-import Data.List
-  ( foldl',
-    foldl1',
-    isPrefixOf,
-    iterate,
-  )
+import Data.Functor ((<&>))
+import Data.Ix (inRange, range)
+import Data.List (foldl')
 import Data.List qualified as L
-import Data.Map.Lazy (Map)
-import Data.Map.Lazy qualified as M
-import Data.Maybe
-  ( catMaybes,
-    fromJust,
-    isJust,
-  )
-import Data.Ord (comparing)
-import Data.Sequence
-  ( Seq (..),
-    (<|),
-    (|>),
-  )
+import Data.Maybe (fromJust)
+import Data.Sequence (Seq (..))
 import Data.Sequence qualified as Seq
 import Data.Set (Set)
 import Data.Set qualified as S
-import Data.Vector.Unboxed (Vector)
-import Data.Vector.Unboxed qualified as VU
-import Data.Void (Void)
-import Debug.Trace
-  ( traceShow,
-    traceShowId,
-  )
-import Linear.V2 (V2 (..))
 import Linear.V3 (V3 (..))
-import Linear.V4 (V4 (..))
 import Test.HUnit.Base (Test (TestCase), (@?=))
 import Test.HUnit.Text (runTestTT)
 import Text.ParserCombinators.ReadP qualified as P
@@ -102,35 +59,49 @@ solve1 input =
     & S.fromList
     & exposedSideCount
 
-loeb :: Functor f => f (f a -> a) -> f a
-loeb x = go where go = fmap ($ go) x
-
 withAirPockets :: Set Coord -> Set Coord
 withAirPockets lava =
   bounds
     & range
-    <&> (\c -> (c, isSurrounded c))
-    & M.fromList
-    & loeb
-    & M.filter id
-    & M.keys
-    & S.fromList
+    & foldl' f lava
   where
-    isSurrounded !c !m
-      | c `S.member` lava = True
-      | not (all (inRange bounds) (neighbors c)) = False
-      | otherwise = all (m M.!) (neighbors c)
-    bounds =
-      ( V3 (minimum (x <$> lavaList)) (minimum (y <$> lavaList)) (minimum (z <$> lavaList)),
-        V3 (maximum (x <$> lavaList)) (maximum (y <$> lavaList)) (maximum (z <$> lavaList))
-      )
-      where
-        lavaList = S.toList lava
-        x (V3 x _ _) = x
-        y (V3 _ y _) = y
-        z (V3 _ _ z) = z
+    f lava c = S.union lava (isBubble lava c)
+    bounds = lavaBounds lava
 
-solve2 :: [Coord] -> _
+isBubble :: Set Coord -> Coord -> Set Coord
+isBubble lava c = go c Seq.empty (S.singleton c)
+  where
+    go :: Coord -> Seq Coord -> Set Coord -> Set Coord
+    go c toVisit explored
+      | not (inRange bounds c) = S.empty
+      | otherwise =
+          case newToVisit of
+            Empty -> explored
+            x :<| xs -> go x xs newExplored
+      where
+        newToVisit =
+          newToExplore
+            & Seq.fromList
+            & (toVisit Seq.><)
+        newToExplore =
+          neighbors c
+            & filter (not . (`S.member` explored))
+            & filter (not . (`S.member` lava))
+        newExplored = S.union explored (S.fromList newToExplore)
+    bounds = lavaBounds lava
+
+lavaBounds :: Set Coord -> (Coord, Coord)
+lavaBounds lava =
+  ( V3 (minimum (x <$> lavaList)) (minimum (y <$> lavaList)) (minimum (z <$> lavaList)),
+    V3 (maximum (x <$> lavaList)) (maximum (y <$> lavaList)) (maximum (z <$> lavaList))
+  )
+  where
+    lavaList = S.toList lava
+    x (V3 x _ _) = x
+    y (V3 _ y _) = y
+    z (V3 _ _ z) = z
+
+solve2 :: [Coord] -> Int
 solve2 input =
   input
     & S.fromList
@@ -140,10 +111,9 @@ solve2 input =
 main = do
   input <- readFile "inputs/Day18.txt"
   exampleInput <- readFile "inputs/Day18_example.txt"
-  print $ solve2 $ parse input
   runTestTT $
     TestCase $ do
       solve1 (parse exampleInput) @?= 64
       solve1 (parse input) @?= 4548
-      -- solve2 (parse exampleInput) @?= 58
-      -- solve2 (parse input) @?= 58
+      solve2 (parse exampleInput) @?= 58
+      solve2 (parse input) @?= 2588
