b9876c19c4d395cf2f6948071e7cdd343417e7da
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2024-12-04 11:52:52 +0200

    Solve day 4

---
+++ 2024/Day04.hs
@@ -0,0 +1,97 @@
+module Day04 (main) where
+
+import Data.Array.IArray qualified as A
+import Data.Char qualified as C
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.Ix (inRange, range)
+import Data.List qualified as L
+import Data.Maybe (fromJust)
+import Linear.V2 (V2 (..))
+import Test.HUnit.Base (Test (TestCase), (@?=))
+import Test.HUnit.Text (runTestTT)
+import Text.ParserCombinators.ReadP qualified as P
+
+parse :: String -> [[Char]]
+parse input = run $ (P.many1 letter `P.endBy1` eol) <* P.eof
+  where
+    -- Standard parsers
+    letter = P.satisfy C.isLetter
+    eol = P.char '\n'
+    run p = fullMatch $ P.readP_to_S p input
+    fullMatch :: [(a, [b])] -> a
+    fullMatch = fst . fromJust . L.find (L.null . snd)
+
+type Coord = V2 Int
+
+type Grid = A.Array Coord Char
+
+createGrid :: [[Char]] -> Grid
+createGrid rows = A.listArray bounds $ concat rows
+  where
+    bounds = (V2 0 0, V2 (length rows - 1) (length (head rows) - 1))
+
+type Path = [V2 Int]
+
+xmasPaths :: [Path]
+xmasPaths =
+  [ V2 0 1, -- right
+    V2 1 1, -- down right
+    V2 1 0, -- down
+    V2 1 (-1), -- down left
+    V2 0 (-1), -- left
+    V2 (-1) (-1), -- up left
+    V2 (-1) 0, -- up
+    V2 (-1) 1 -- up right
+  ]
+    <&> \dir -> take 4 $ iterate (+ dir) (V2 0 0)
+
+crossMAS :: Path
+crossMAS =
+  [ V2 (-1) (-1), -- up left
+    V2 0 0, -- center
+    V2 1 1, -- down right
+    V2 (-1) 1, -- up right
+    V2 0 0, -- center
+    V2 1 (-1) -- down left
+  ]
+
+cartProd :: [a] -> [a] -> [(a, a)]
+cartProd xs ys = [(x, y) | x <- xs, y <- ys]
+
+selfCartProd :: [a] -> [(a, a)]
+selfCartProd xs = cartProd xs xs
+
+solve1 :: [[Char]] -> Int
+solve1 input =
+  candidates
+    & filter (all (inRange bounds))
+    & filter isXMAS
+    & length
+  where
+    grid = createGrid input
+    bounds = A.bounds grid
+    xCoords = filter (\coord -> grid A.! coord == 'X') $ range bounds
+    candidates = (\coord -> fmap (coord +) <$> xmasPaths) `concatMap` xCoords
+    isXMAS cs = ((grid A.!) <$> cs) == "XMAS"
+
+solve2 :: [[Char]] -> Int
+solve2 input =
+  candidates
+    & filter (all (inRange bounds))
+    & filter isCrossMAS
+    & length
+  where
+    grid = createGrid input
+    bounds = A.bounds grid
+    aCoords = filter (\coord -> grid A.! coord == 'A') $ range bounds
+    candidates = (\coord -> (coord +) <$> crossMAS) <$> aCoords
+    isCrossMAS cs = ((grid A.!) <$> cs) `elem` crossMASCombinations
+    crossMASCombinations = uncurry (++) <$> selfCartProd ["MAS", "SAM"]
+
+main = do
+  input <- readFile "inputs/Day04.txt"
+  runTestTT $
+    TestCase $ do
+      solve1 (parse input) @?= 2406
+      solve2 (parse input) @?= 1807
