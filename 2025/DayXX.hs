1605eb03739ba4a5e69e70c2e823f848d12e8911
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2025-11-30 18:14:32 +0200

    Prep 2025

---
+++ 2025/DayXX.hs
@@ -0,0 +1,105 @@
+module DayXX (main) where
+
+import Control.Applicative (empty, (<|>))
+import Control.Arrow (first, second, (>>>))
+import Control.Monad (guard)
+import Criterion.Main
+  ( bench,
+    defaultMain,
+    whnf,
+  )
+import Data.Array.IArray (Array)
+import qualified Data.Array.IArray as A
+import qualified Data.Char as C
+import Data.Function ((&))
+import Data.Functor ((<$), (<&>), ($>))
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IS
+import Data.Ix
+  ( inRange,
+    range,
+  )
+import Data.List
+  ( foldl',
+    foldl1',
+    isPrefixOf,
+    iterate,
+  )
+import qualified Data.List as L
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.HashMap.Strict (HashMap)
+import Data.HashMap.Strict qualified as HM
+import Data.Maybe
+  ( catMaybes,
+    fromJust,
+    isJust,
+  )
+import Data.Ord (comparing)
+import Data.Sequence
+  ( Seq (..),
+    (<|),
+    (|>),
+  )
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as VU
+import Data.Void (Void)
+import Debug.Trace
+  ( traceShow,
+    traceShowId,
+  )
+import Linear.V2 (V2 (..))
+import Linear.V3 (V3 (..))
+import Linear.V4 (V4 (..))
+import Test.HUnit.Base (Test (TestCase), (@?=))
+import Test.HUnit.Text (runTestTT)
+import qualified Text.ParserCombinators.ReadP as P
+import Control.Monad.Combinators qualified as PC
+
+parse :: String -> _
+parse input = run $ do
+  word <- P.many1 letter <* P.char ':' <* eol
+  numbers <- signum `P.sepBy1` spaces
+  -- eol *> P.eof
+  return (word, numbers)
+  where
+    -- Standard parsers
+    letter = P.satisfy C.isLetter
+    signum = do
+      sign <- P.option 1 ((-1) <$ P.char '-')
+      number <- number
+      return (sign * number)
+    number :: P.ReadP Int
+    number = read <$> P.munch1 C.isDigit
+    spaces = P.many1 (P.char ' ')
+    eol = P.char '\n'
+    run p = longestMatch $ P.readP_to_S p input
+    longestMatch :: [(a, [b])] -> (a, [b])
+    longestMatch = L.minimumBy (comparing (length . snd))
+    fullMatch :: [(a, [b])] -> a
+    fullMatch = fst . fromJust . L.find (L.null . snd)
+
+solve1 :: _
+solve1 input =
+  input
+
+solve2 :: _
+solve2 input =
+  input
+
+main = do
+  input <- readFile "inputs/DayXX.txt"
+  -- exampleInput <- readFile "inputs/DayXX_example.txt"
+  print $ solve1 $ parse input
+  -- print $ solve2 $ parse input
+  runTestTT $
+    TestCase $ do
+      1 @?= 2
+      1 @?= 1
+      -- solve1 (parse input) @?= 2
+      -- solve2 (parse input) @?= 1
---
+++ 2025/prep.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [ $# -eq 0 ]; then
+  if [ "$(TZ='America/New_York' date '+%H')" -ge 23 ]; then
+    day="$(TZ='America/New_York' date -v '+1d' '+%d' | sed 's/^0*//' | xargs printf '%02d')"
+  else
+    day="$(TZ='America/New_York' date '+%d' | sed 's/^0*//' | xargs printf '%02d')"
+  fi
+
+  echo "Creating code file ..."
+  cp DayXX.hs "Day${day}.hs"
+  sed -i '' -e "s/XX/${day}/" "Day${day}.hs"
+
+  echo "Waiting for input .."
+  mkdir -p inputs
+  if [ "$(TZ='America/New_York' date '+%H')" -ge 23 ]; then
+    aocdl -output "inputs/Day${day}.txt" -wait
+  else
+    aocdl -output "inputs/Day${day}.txt"
+  fi
+
+  echo "Watching the main function .."
+  ghcid --test='main' -W -c "stack ghci --ghc-options='-w +RTS -N8 -RTS' Day${day}.hs"
+elif [ $# -eq 1 ]; then
+  day="$(printf '%02d' "$1")"
+
+  echo "Creating code file ..."
+  cp DayXX.hs "Day${day}.hs"
+  sed -i '' -e "s/XX/${day}/" "Day${day}.hs"
+
+  echo "Waiting for input .."
+  mkdir -p inputs
+  aocdl -output "inputs/Day${day}.txt"
+
+  echo "Watching the main function .."
+  ghcid --test='main' -W -c "stack ghci --ghc-options='-w +RTS -N8 -RTS' Day${day}.hs"
+elif [ $# -eq 2 ]; then
+  year="$1"
+  day="$(printf '%02d' "$2")"
+  name="Y${year}D${day}"
+
+  echo "Creating code file ..."
+  mkdir -p "Practice"
+  cp DayXX.hs "Practice/${name}.hs"
+  sed -i '' -e "s/module DayXX/module Practice.${name}/" "Practice/${name}.hs"
+  sed -i '' -e "s|inputs/DayXX|Practice/inputs/${name}|" "Practice/${name}.hs"
+
+  echo "Waiting for input .."
+  mkdir -p "Practice/inputs"
+  aocdl -output "Practice/inputs/${name}.txt" -day "$day" -year "$year"
+
+  echo "Watching the main function .."
+  ghcid --test='main' -W -c "stack ghci --ghc-options='-w +RTS -N8 -RTS' Practice/${name}.hs"
+else
+  echo "I don't understand" >&2
+  exit 1
+fi
